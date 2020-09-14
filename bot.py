3fa81bbd1dd02136648aa84e36976705c2ec7630
Your Name <you@example.com>
2020-09-14 20:58:01 +0800

    thats not working:(

--- bot.py
+++ bot.py
@@ -2,8 +2,11 @@ import telebot
 import cfg
 import urllib
 import os
+from telebot import types
 
 bot = telebot.TeleBot(cfg.token)
+
+flag = False
 
 
 ############# START COMMAND ################
@@ -13,13 +16,14 @@ def start(message):
     bot.send_message(message.chat.id, hi_message)
 
 
-############# FILE CHECK AND DOWNLOAD ################
+############# FILE CHECK ################
 @bot.message_handler(content_types=["document"])
 def handle_docs(message):
     DB(message)
+
     if message.document.file_size >= 20971520:
         print('too big')
-        print(message.document)                    #Is file too big?
+        print(message.document)                                #Is file too big?
         toobig(message)
     else:
         document_id = message.document.file_id
@@ -27,12 +31,13 @@ def handle_docs(message):
         useless, file_extension = os.path.splitext(file_info.file_path)
         if file_extension not in cfg.allowedfiles:            #Is file unsupported
             wrong_extension(file_extension, message)
-        else:                                                 #All is fine lets download
-            file_path = 'documents/' + str(message.from_user.id) + str(message.message_id) + str(file_extension)
-            link = 'https://api.telegram.org/file/bot' + cfg.token + '/' + str(file_info.file_path)
-            urllib.request.urlretrieve(link, file_path)
-            bot.send_message(message.chat.id, 'Success. You did it')
-
+        else:                                                 #User opinion
+            areusure(message)
+            global flag
+            while flag == True:
+                download(message, file_extension, file_info)
+                print('Флаг опускаем')
+                flag = False
 
 ############# HELP COMMAND ################
 @bot.message_handler(commands=['help'])
@@ -63,6 +68,50 @@ def toobig(message):
 def wrong_extension(file_extension, message):
     wrong_message = 'Sorry but '+file_extension+' type file doesnt supports. Try other filetype'
     bot.send_message(message.chat.id, wrong_message)
+
+
+############# BUTTONS TEST ################
+@bot.message_handler(commands = ['url'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    mylovestbutton = types.InlineKeyboardButton(text='Wanna check smth?', url='https://github.com/doesitworkornot?tab=repositories')
+    markup.add(mylovestbutton)
+    bot.send_message(message.chat.id, "you'll like it", reply_markup=markup)
+
+
+############# DOWNLOAD ################
+def download(message, file_extension, file_info):
+    file_path = 'documents/' + str(message.from_user.id) + str(message.message_id) + str(file_extension)
+    link = 'https://api.telegram.org/file/bot' + cfg.token + '/' + str(file_info.file_path)
+    urllib.request.urlretrieve(link, file_path)
+    bot.send_message(message.chat.id, 'Success. You did it')
+
+
+############# USER CHECK ################
+def areusure(message):
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='Yes', callback_data='yes')
+    keyboard.add(key_yes)
+    key_no= types.InlineKeyboardButton(text='No', callback_data='no')
+    keyboard.add(key_no)
+    key_not_one= types.InlineKeyboardButton(text='Not one', callback_data='not_one')
+    keyboard.add(key_not_one)
+    bot.send_message(message.chat.id, text='Are you sure you want to print one copy', reply_markup=keyboard)
+
+
+############# DATA CHECK ################
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == 'yes':
+        global flag
+        flag = True
+        print('Флаг сейчас:', flag)
+    elif call.data == 'not_one':
+        bot.send_message(call.message.chat.id, 'How much?')
+    elif call.data == 'no':
+        bot.send_message(call.message.chat.id, 'Ok then')
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+
 
 
 bot.polling()
